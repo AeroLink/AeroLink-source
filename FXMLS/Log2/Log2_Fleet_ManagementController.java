e16c3660161f867f757e42958c8f9c38a8c87ed4
delle <randelletolentino@gmail.com>
2018-08-25 12:31:02 -0800

    log2 adding of module design, combox content

--- FXMLS/Log2/Log2_Fleet_ManagementController.java
+++ FXMLS/Log2/Log2_Fleet_ManagementController.java
@@ -5,9 +5,19 @@
  */
 package FXMLS.Log2;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXComboBox;
+import com.jfoenix.controls.JFXDatePicker;
+import com.jfoenix.controls.JFXTextField;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.TableView;
+import javax.swing.JOptionPane;
 
 /**
  * FXML Controller class
@@ -16,12 +26,47 @@ import javafx.fxml.Initializable;
  */
 public class Log2_Fleet_ManagementController implements Initializable {
 
-    /**
-     * Initializes the controller class.
-     */
+    @FXML
+    private JFXComboBox<String> item_type;
+
+    @FXML
+    private JFXButton btn_save;
+    
+    
+    
+    // Item Type ... items
+      ObservableList<String> it = FXCollections.observableArrayList("Fan" , "Table" , "Chair");
+    @FXML
+    private TableView<?> im_tb;
+    @FXML
+    private JFXTextField txt_transacno;
+    @FXML
+    private JFXTextField txt_personnelincharge;
+    @FXML
+    private JFXDatePicker date_recieved;
+      
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        // combobox
+        item_type.setItems(it);
+        item_type.setPromptText("Choose Item type");
+        DisableComponents();
+        
+        btn_save.setOnMouseClicked(e -> Save());
+       
+        
+    }
+        
+        public void DisableComponents(){
+            
+        
+        
     }    
-    
+     
+        public void Save(){
+            
+        Helpers.AlertResponse alert = new Helpers.AlertResponse(Alert.AlertType.INFORMATION, "Message", "Ginagawamue","");
+            alert.open();    
+       
+        }
 }
