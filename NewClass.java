788ef4c08085677670c5a6073f2bfd91a1a98831
lei <lecias.ariel@gmail.com>
2018-08-29 15:45:28 +0800

    Synthesize new function in model class

--- NewClass.java
+++ NewClass.java
@@ -1,9 +1,11 @@
 
 import Model.Sample;
+import Model.UserPermissions;
 import Model.Users;
 import Synapse.Crypt;
 import Synapse.DB.MySql;
 import Synapse.Database;
+import Synapse.Model;
 import java.util.Arrays;
 import java.util.List;
 
@@ -21,26 +23,24 @@ public class NewClass {
     
     public static void main(String[] args){
         
-        //test
+        //Declaring new Instance of Database
         Database db =  Database.getInstance();
         db.DB_INIT(new MySql());
+        
+        //initialize DB Connection
         db.startConnection();
         
-
-        //new 
-        Users u = new Users();
+        //Please, Don't mess up > < ! .. 
+        //God Bless this Code ..
         
-        //u.insert(0, "adminxaasasaaa", Crypt.Encrypt("edenramoneda"));
-        
-        List list = u.where("id", "like", "%1%").get();
+        UserPermissions up = new UserPermissions();
+        List list = up
+                .join(Model.JOIN.INNER, "tbl_users", "id", "=", "user_id")
+                .join(Model.JOIN.INNER, "tbl_permissions", "id", "=", "permission_id")
+                .get("tbl_users.id", "GROUP_CONCAT(tbl_permissions.permission) as permissions");
         
         System.out.println(Arrays.asList(list));
-//        
-//        u.update(new Object[][] {
-//            {"username", "admin"}
-//        }).where("id", "=", "19").executeUpdate();
 
-        //u.delete().where("id", "=", "20").executeUpdate();
         
     }
     
