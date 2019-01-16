b480931b501f0819bde19fde62b6e496206ae045
bertopogi <bertogilberto31@gmail.com>
2019-01-17 07:02:40 +0800

    update again

--- Config/DatabaseConfig.java
+++ Config/DatabaseConfig.java
@@ -20,9 +20,9 @@ public class DatabaseConfig {
 
     //if offline mode
     public static Boolean offline = true;
-    public static String IP = "192.168.100.34"; //NS558185\AEROLINKSQL
-    public static String PORT = "1444";
+    public static String IP = "127.0.0.1"; //NS558185\AEROLINKSQL      //192.168.100.34
+    public static String PORT = "1433";
     public static String DATABASE = "AeroLink";
-    public static String USER = "4205";
-    public static String PASSWORD = "3d3nr4m0n3d4";
+    public static String USER = "finance";
+    public static String PASSWORD = "finance";
 }
