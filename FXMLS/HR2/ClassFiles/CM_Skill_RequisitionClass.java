f91b72e0bf8234f6afa5d61d423a98b576ca9dfb
edenlee143@gmail.com <edenlee143@gmail.com>
2019-01-24 17:00:28 +0800

    add requisition inbox in competency management

---
+++ FXMLS/HR2/ClassFiles/CM_Skill_RequisitionClass.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package FXMLS.HR2.ClassFiles;
+
+import javafx.beans.property.SimpleStringProperty;
+
+/**
+ *
+ * @author EdenRamoneda
+ */
+public class CM_Skill_RequisitionClass {
+    
+    public SimpleStringProperty sr_id, dept_name, title, req_status;
+    
+    public CM_Skill_RequisitionClass(String sr_id, String dept_name, String title, String req_status)
+    {
+        this.sr_id = new SimpleStringProperty(sr_id);
+        this.dept_name = new SimpleStringProperty(dept_name);
+        this.title = new SimpleStringProperty(title);
+        this.req_status = new SimpleStringProperty(req_status);
+    }
+    
+}
