b245498750b79f0c11f7c73a1712af524597f0b5
edenlee143@gmail.com <edenlee143@gmail.com>
2019-01-29 23:35:17 +0800

    display list of vehicles in training management

---
+++ FXMLS/HR2/Modals/TM_ViewFacilityDetailsController.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package FXMLS.HR2.Modals;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+/**
+ * FXML Controller class
+ *
+ * @author EdenRamoneda
+ */
+public class TM_ViewFacilityDetailsController implements Initializable {
+
+    @FXML
+    private Label lbl_RoomNumber;
+    @FXML
+    private Label lbl_facName;
+    @FXML
+    private Label lbl_BName;
+    @FXML
+    private Label lbl_Capacity;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
