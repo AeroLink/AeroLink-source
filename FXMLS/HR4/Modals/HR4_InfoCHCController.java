0af5c24defabc9b7e163bebcb701a6c1928fe288
YouJaezz <jaejae.queral@gmail.com>
2019-02-03 10:34:19 +0800

    HR4 Update 10:34 am

--- FXMLS/HR4/Modals/HR4_InfoCHCController.java
+++ FXMLS/HR4/Modals/HR4_InfoCHCController.java
@@ -9,7 +9,11 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 
 /**
  * FXML Controller class
@@ -17,6 +21,51 @@ import javafx.scene.control.Label;
  * @author Jaeeeee
  */
 public class HR4_InfoCHCController implements Initializable {
+
+    @FXML
+    private Tab tbl_pers;
+    @FXML
+    private TextField ln;
+    @FXML
+    private TextField fn;
+    @FXML
+    private TextField mn;
+    @FXML
+    private TextField suffix;
+    @FXML
+    private TextField gender;
+    @FXML
+    private TextField cs;
+    @FXML
+    private TextField wei;
+    @FXML
+    private TextField hei;
+    @FXML
+    private TextField cont_no;
+    @FXML
+    private TextField email;
+    @FXML
+    private TextField adds;
+    @FXML
+    private DatePicker dob;
+    @FXML
+    private DatePicker pob;
+    @FXML
+    private Label ec;
+    @FXML
+    private TableView<?> tbl_fb;
+    @FXML
+    private TableView<?> tbl_educ;
+    @FXML
+    private TableView<?> tbl_sems;
+    @FXML
+    private TableView<?> tbl_works;
+    @FXML
+    private Tab tbl_certs;
+    @FXML
+    private TableView<?> tbl_awards;
+    @FXML
+    private TableView<?> tbl_govs;
 
 
     /**
