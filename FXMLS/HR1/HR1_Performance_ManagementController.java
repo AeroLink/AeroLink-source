93675cae8e79fa1d20224d0404482488c07d1bc8
lei <lecias.ariel@gmail.com>
2019-02-03 22:51:59 +0800

    Performance Management FIXED

--- FXMLS/HR1/HR1_Performance_ManagementController.java
+++ FXMLS/HR1/HR1_Performance_ManagementController.java
@@ -147,9 +147,7 @@ public class HR1_Performance_ManagementController implements Initializable {
         ObservableList<TableModel_EmployeeList> list = FXCollections.observableArrayList();
 
 //        STORED_PROC.executeCall("HR1_PERFOMANCE_MODULE_getAllEmployees", new Object[][]{
-//            {"dept", cboDept.getSelectionModel().getSelectedItem().toString()},
-//            {"type_id",  5},
-//            {"status_id", 1}
+//
 //        }).stream().forEach(action -> {
 //
 //            HashMap row = (HashMap) action;
@@ -169,7 +167,10 @@ public class HR1_Performance_ManagementController implements Initializable {
                 .join(Model.JOIN.INNER, "aerolink.tbl_hr4_employees", "employee_code", "=", "employee_code")
                 .join(Model.JOIN.INNER, "aerolink.tbl_hr4_jobs", "job_id", "=", "job_id")
                 .join(Model.JOIN.INNER, "aerolink.tbl_hr4_department", "id", "=", "aerolink.tbl_hr4_jobs", "dept_id", true)
-                .where(new Object[][]{}).get()
+                .where(new Object[][]{
+                    {"dept_name", "=", cboDept.getSelectionModel().getSelectedItem().toString()},
+                    {"status_id", "=", 1}
+                }).get()
                 .stream().forEach(action -> {
 
                     HashMap row = (HashMap) action;
