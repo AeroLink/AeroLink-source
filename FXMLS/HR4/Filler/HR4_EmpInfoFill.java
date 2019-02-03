d75cda7f8b15c0a4cb905afd79b17e57f4f3be47
YouJaezz <jaejae.queral@gmail.com>
2019-02-03 09:21:21 +0800

    HR4 Update 9am

---
+++ FXMLS/HR4/Filler/HR4_EmpInfoFill.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package FXMLS.HR4.Filler;
+
+/**
+ *
+ * @author Jaeeeee
+ */
+public class HR4_EmpInfoFill {
+    
+}
